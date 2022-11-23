0f279188756d04211533a34482426ffb9fee3823
YanxiaWu <yanxiawusilva@gmail.com>
2022-11-23 16:59:54 +0100

    list event

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -50,18 +50,32 @@ router.post("/recetas/crear", (req, res, next) => {
 //listado de recetas
 
 router.get('/recetas', (req, res) => {
-
+    let isAdmin
+    if (req.session.currentUser) {
+        isAdmin = req.session.currentUser.role === 'ADMIN'
+    } else {
+        isAdmin = false
+    }
     Recipe
         .find()
         .then(recipes => {
-            res.render('recipe/list', { recipes: recipes })
+            res.render('recipe/list', { recipes, isAdmin })
         })
         .catch(err => console.log(err))
 })
 
 
 
+//eliminar recetas
+router.post('/recetas/:id/eliminar', (req, res, next) => {
+    const { id: recipe_id } = req.params
 
+    Recipe
+        .findByIdAndDelete(recipe_id)
+        .then(() => res.redirect(`/recetas`))
+        .catch(err => console.log(err))
+
+});
 
 
 
