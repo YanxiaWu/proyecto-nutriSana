49122a6bd3fb840c0ef7654cf929b5742300ae4e
YanxiaWu <yanxiawusilva@gmail.com>
2022-11-23 17:08:46 +0100

    recetas list

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -30,13 +30,12 @@ router.post("/recetas/crear", (req, res, next) => {
     api
         .getRecipe(recipeJSON)
         .then(apiResponse => {
+            // res.render('recipe/pintar-recipe', { result: apiResponse.data })
             const { ingredients, calories } = apiResponse.data
-            const composion = ingredients.map(ing => ing.text)
-            console.log("hiiiiiiiiiiiii", composion)
-
+            console.log("hiiiiiiiiiiiii", ingredient2)
             const { CHOCDF, FAT, PROCNT } = apiResponse.data.totalNutrients
             Recipe
-                .create({ ingredients: composion, calories, carbohydrate: CHOCDF.quantity, fat: FAT.quantity, protein: PROCNT.quantity })
+                .create({ ingredients, calories, carbohydrate: CHOCDF.quantity, fat: FAT.quantity, protein: PROCNT.quantity })
                 .then(recipe => {
                     res.redirect('/recetas')
                 })
@@ -51,18 +50,32 @@ router.post("/recetas/crear", (req, res, next) => {
 //listado de recetas
 
 router.get('/recetas', (req, res) => {
-
+    let isAdmin
+    if (req.session.currentUser) {
+        isAdmin = req.session.currentUser.role === 'ADMIN'
+    } else {
+        isAdmin = false
+    }
     Recipe
         .find()
         .then(recipes => {
-            res.render('recipe/list', { recipes: recipes })
+            res.render('recipe/list', { recipes, isAdmin })
         })
         .catch(err => console.log(err))
 })
 
 
 
+//eliminar recetas
+router.post('/recetas/:id/eliminar', (req, res, next) => {
+    const { id: recipe_id } = req.params
 
+    Recipe
+        .findByIdAndDelete(recipe_id)
+        .then(() => res.redirect(`/recetas`))
+        .catch(err => console.log(err))
+
+});
 
 
 
